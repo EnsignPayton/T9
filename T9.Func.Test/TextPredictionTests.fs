55a042da4ce3b2699f50f2a63d04971cf20a2ab2
Logan Payton <Logan.Payton@javs.com>
2019-01-05 17:32:27 -0500

    Add f sharp version

---
+++ T9.Func.Test/TextPredictionTests.fs
@@ -0,0 +1,49 @@
+﻿module TextPredictionTests
+
+open Xunit
+open TextPrediction
+
+let test digits expected =
+    let actual = getLetterCombinations digits
+    Assert.Equal<seq<string>>(expected, actual)
+
+[<Fact>]
+let ``Returns correct combos for 2`` () =
+    test "2" ["a"; "b"; "c"]
+
+[<Fact>]
+let ``Returns correct combos for 3`` () =
+    test "3" ["d"; "e"; "f"]
+
+[<Fact>]
+let ``Returns correct combos for 4`` () =
+    test "4" ["g"; "h"; "i"]
+
+[<Fact>]
+let ``Returns correct combos for 5`` () =
+    test "5" ["j"; "k"; "l"]
+
+[<Fact>]
+let ``Returns correct combos for 6`` () =
+    test "6" ["m"; "n"; "o"]
+
+[<Fact>]
+let ``Returns correct combos for 7`` () =
+    test "7" ["p"; "q"; "r"; "s"]
+
+[<Fact>]
+let ``Returns correct combos for 8`` () =
+    test "8" ["t"; "u"; "v"]
+
+[<Fact>]
+let ``Returns correct combos for 9`` () =
+    test "9" ["w"; "x"; "y"; "z"]
+
+[<Fact>]
+let ``Returns example double digit combinations`` () =
+    test "23" ["ad"; "ae"; "af"; "bd"; "be"; "bf"; "cd"; "ce"; "cf"]
+
+[<Fact>]
+let ``Returns baby got back`` () =
+    let actual = getLetterCombinations "6492568"
+    Assert.Contains("mixalot", actual)
---
+++ T9.Func/TextPrediction.fs
@@ -0,0 +1,36 @@
+﻿module TextPrediction
+
+let private _mappings =
+    [
+        2, "abc";
+        3, "def";
+        4, "ghi";
+        5, "jkl";
+        6, "mno";
+        7, "pqrs";
+        8, "tuv";
+        9, "wxyz";
+    ] |> Map.ofList
+
+let private getCombos digit =
+    _mappings.[digit]
+    |> Seq.map (fun c -> string c)
+
+let private getCombosAppend digit str =
+    _mappings.[digit]
+    |> Seq.map (fun c -> str + string c)
+
+let private foldDigits acuum digit =
+    acuum |> Seq.collect (fun partial -> getCombosAppend digit partial)
+
+let private _getLetterCombinations (digits : seq<int>) : seq<string> =
+    let first = (digits
+        |> Seq.head
+        |> getCombos)
+    digits
+    |> Seq.skip 1
+    |> Seq.fold foldDigits first
+
+let getLetterCombinations (digits : string) : seq<string> =
+    let intDigits = digits |> Seq.map (fun d -> int d - int '0')
+    _getLetterCombinations intDigits
