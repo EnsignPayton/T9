a5290592d7d18e6e29647f1f9c57869bcb17e12b
Logan Payton <Logan.Payton@javs.com>
2019-01-05 15:41:21 -0500

    Initial commit

---
+++ T9.Test/TextPredictorTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace T9.Test
+{
+    public class TextPredictorTests
+    {
+        [Fact]
+        public void RejectsNullInput()
+        {
+            var target = new TextPredictor();
+            Assert.Throws<ArgumentNullException>(() => target.GetLetterCombinations(null));
+        }
+
+        [Fact]
+        public void RejectsOutOfRangeInput()
+        {
+            var target = new TextPredictor();
+            Assert.Throws<FormatException>(() => target.GetLetterCombinations("cat"));
+        }
+
+        [Theory]
+        [InlineData("2", new[] { "a", "b", "c" })]
+        [InlineData("3", new[] { "d", "e", "f" })]
+        [InlineData("4", new[] { "g", "h", "i" })]
+        [InlineData("5", new[] { "j", "k", "l" })]
+        [InlineData("6", new[] { "m", "n", "o" })]
+        [InlineData("7", new[] { "p", "q", "r", "s" })]
+        [InlineData("8", new[] { "t", "u", "v" })]
+        [InlineData("9", new[] { "w", "x", "y", "z" })]
+        public void ReturnsSingleDigitCombinations(string input, IEnumerable<string> expected)
+        {
+            var target = new TextPredictor();
+
+            var actual = target.GetLetterCombinations(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReturnsExampleDoubleDigitCombinations()
+        {
+            var expected = new[] { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" };
+
+            var target = new TextPredictor();
+
+            var actual = target.GetLetterCombinations("23");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReturnsBabyGotBack()
+        {
+            const string expected = "mixalot";
+
+            var target = new TextPredictor();
+
+            var actual = target.GetLetterCombinations("6492568");
+
+            Assert.Contains(expected, actual);
+        }
+    }
+}
---
+++ T9/TextPredictor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace T9
+{
+    public class TextPredictor
+    {
+        private readonly IDictionary<int, string> _mappings = new Dictionary<int, string>
+        {
+            { 2, "abc" },
+            { 3, "def" },
+            { 4, "ghi" },
+            { 5, "jkl" },
+            { 6, "mno" },
+            { 7, "pqrs" },
+            { 8, "tuv" },
+            { 9, "wxyz" }
+        };
+
+        // Given a string containing digits from 2-9 inclusive,
+        // return all possible letter combinations that the number could represent.
+        public IEnumerable<string> GetLetterCombinations(string digits)
+        {
+            var intDigits = digits
+                .Select(d => int.Parse(d.ToString()));
+
+            return GetLetterCombinations(intDigits);
+        }
+
+        private IEnumerable<string> GetLetterCombinations(IEnumerable<int> digits)
+        {
+            return digits.Skip(1)
+                .Aggregate(GetCombos(digits.First()), (accum, digit) =>
+                    accum.SelectMany(partial => GetCombos(digit, partial)));
+        }
+
+        private IEnumerable<string> GetCombos(int digit, string str = "")
+        {
+            return _mappings[digit].Select(c => str + c);
+        }
+    }
+}
